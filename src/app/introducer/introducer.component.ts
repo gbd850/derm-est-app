8c833af2bc33c982a2c57d084e66baf71e27c171
gbd850 <mmyszaki@gmail.com>
2024-01-23 22:35:28 +0100

    Add first elements to home page
    Add header component
    Add footer component
    Add introducing component

---
+++ src/app/introducer/introducer.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-introducer',
+  standalone: true,
+  imports: [],
+  templateUrl: './introducer.component.html',
+  styleUrl: './introducer.component.scss'
+})
+export class IntroducerComponent {
+
+}
