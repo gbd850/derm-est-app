242a9d1da45e20e107dc7f77fabcccca3cd3efca
gbd850 <mmyszaki@gmail.com>
2024-01-25 14:38:22 +0100

    Add advantages component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,26 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { IntroducerComponent } from './introducer/introducer.component';
+import { ProceduresComponent } from './procedures/procedures.component';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { AdvantagesComponent } from './advantages/advantages.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HeaderComponent, FooterComponent],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    HeaderComponent,
+    FooterComponent,
+    IntroducerComponent,
+    ProceduresComponent,
+    AboutMeComponent,
+    AdvantagesComponent
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'derm-est-app';
