2f2011a2ff04eed6a63d9036fc4e3240365e9e55
gbd850 <mmyszaki@gmail.com>
2024-01-26 20:06:35 +0100

    Add about us page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,14 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { AboutMePageComponent } from './about-us-page/about-me-page.component';
 
-export const routes: Routes = [{
-    path: "", component: HomeComponent
-}];
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  {
+    path: 'o-nas',
+    component: AboutMePageComponent,
+  },
+];
